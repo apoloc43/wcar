d4275d41a17dfd1632d75dd4a83dacf43e974302
apolo <apoloc43@icloud.com>
2023-04-30 23:49:22 -0300

    autenticação

---
+++ aluguel/urls.py
@@ -0,0 +1,33 @@
+"""carro URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import index, lista_carros, realizar_aluguel, detalhar_carro, realizar_aluguel_carro,listar_clientes,listar_alugueis
+from .views import cadastrar_carro,detalhar_cliente,atualizar_carro,deletar_carro
+from aluguel import views
+urlpatterns = [
+    path('registration', views.register, name='register'), 
+    path('', index, name='index'),
+    path('carros/', lista_carros, name='listar_carros'),
+    path('alugueis/', listar_alugueis, name='listar_alugueis'),
+    path('carro/cadastrar', cadastrar_carro, name='cadastrar_carro'),
+    path('carro/atualizar/<int:pk>', atualizar_carro, name='atualizar_carro'),
+    path('carro/deletar/<int:pk>', deletar_carro, name='deletar_carro'),
+    path('cliente/', listar_clientes, name='listar_clientes'),
+    path('cliente/<int:pk>', detalhar_cliente, name='detalhar_cliente'),
+    path('carros/<int:pk>', detalhar_carro, name='detalhar_carro'),
+    path('carros/aluguel/<int:carro_pk>', realizar_aluguel_carro, name='realizar_aluguel_carro' ),
+    path('aluguel/add', realizar_aluguel, name='realizar_aluguel'),
+]
---
+++ aluguel/views.py
@@ -0,0 +1,123 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Carro, Aluguel,Cliente
+from .forms import AluguelForm,CarroForm
+from .admin import CustomUserCreationForm
+from django.contrib import messages
+
+
+# Create your views here.
+
+def index(request):
+    carros = Carro.objects.all()[:5]
+    return render(request, 'index.html', {"carros":carros})
+
+def lista_carros(request):
+    carros = Carro.objects.all()
+    return render(request, 'carro/listar.html', {"carros":carros})
+
+def detalhar_carro(request, pk):
+    carro = Carro.objects.get(pk=pk)
+    return render(request, 'carro/detalhar.html', {"carro":carro})
+@login_required 
+def cadastrar_carro(request):
+    if request.method == "POST":
+        form = CarroForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form = CarroForm()
+            return render(request, "carro/cadastrar.html" , {'form': form})
+    else:
+        form = CarroForm()
+        return render(request, "carro/cadastrar.html", {'form': form})
+@login_required 
+def atualizar_carro(request, pk):
+    carro = Carro.objects.get(pk=pk)
+    form = CarroForm(instance=carro)
+    
+    if request.method == "POST":
+        form = CarroForm(request.POST, request.FILES, instance=carro)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, "carro/atualizar.html", {'form': form})
+    else:
+        return render(request, "carro/atualizar.html", {'form': form})
+@login_required     
+def deletar_carro(request, pk):
+    carro = Carro.objects.get(pk=pk)
+
+    if carro:
+        carro.delete()
+        return redirect("/")
+    else:
+        return render(request, "carro/listar.html", {'msg': "carro não encontrado"})
+
+@login_required 
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/listar.html', {"clientes":clientes})
+@login_required 
+def detalhar_cliente(request, pk):
+    cliente = Cliente.objects.get(pk=pk)
+    return render(request, 'cliente/detalhar.html', {"cliente":cliente})
+@login_required 
+def listar_alugueis(request):
+    alugueis = Aluguel.objects.all()
+    return render(request, 'aluguel/listar.html', {"alugueis":alugueis})
+
+    
+
+@login_required 
+def realizar_aluguel(request):
+    if request.method == "POST":
+        form = AluguelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form = AluguelForm()
+            return render(request, "aluguel/cadastrar.html", {'form': form})
+    else:
+        form = AluguelForm()
+        return render(request, "aluguel/cadastrar.html", {'form': form})
+@login_required 
+def realizar_aluguel_carro(request, carro_pk):
+    carro = Carro.objects.get(pk=carro_pk)
+    aluguel = Aluguel()
+    aluguel.carro = carro
+    
+    form = AluguelForm(instance=aluguel)
+    if request.method == "POST":
+        form = AluguelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form = AluguelForm(instance=aluguel)
+            return render(request, "aluguel/cadastrar.html", {'form': form})
+    else:
+        form = AluguelForm(instance=aluguel)
+        return render(request, "aluguel/cadastrar.html", {'form': form})
+    
+
+def register(request):
+        form = CustomUserCreationForm()
+        if request.method == "POST":
+            form = CustomUserCreationForm(request.POST)
+    
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_valid = False
+                user.save()
+                messages.success(request, 'Registrado. Agora faça o login para começar!')
+                return render(request,'index.html')
+
+            else:
+                print('invalid registration details')
+                
+        return render(request, "registration/register.html",{"form": form})
